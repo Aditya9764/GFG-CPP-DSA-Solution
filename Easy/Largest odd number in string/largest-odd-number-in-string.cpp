d856d5fefe4acf9bc1c329dc3defcc47c64be05d
Aditya9764 <90107321+Aditya9764@users.noreply.github.com>
2024-01-01 18:45:28 +0530

    Updated Solution - GfG to GitHub

--- Easy/Largest odd number in string/largest-odd-number-in-string.cpp
+++ Easy/Largest odd number in string/largest-odd-number-in-string.cpp
@@ -7,10 +7,19 @@ class Solution {
   public:
     string maxOdd(string s) {
         // your code here
-           for(int i=s.length()-1; i>=0; i--)
+       /*    for(int i=s.length()-1; i>=0; i--)
         if((s[i] - 48) % 2 == 1)
             return s.substr(0,i+1);
     return "";
+    */
+          int n=s.size()-1;
+        for(int i=n;i>=0;i--)
+        {
+            int x=s[i]-'0';
+            if(x&1)
+            return s.substr(0,i+1);
+        }
+        return "";
     }
 };
 
